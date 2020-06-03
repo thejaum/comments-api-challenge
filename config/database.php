85c71910ce8d54f416f1e76d3c00f8c8c6863a60
Joao Castro <joao.castro@terceiroslinx.com.br>
2020-06-03 00:18:01 -0300

    Correção no arquivo database.php, estava faltando a linha do migrations.

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Str;
 
 return [
     'default' => env('DB_CONNECTION', 'mysql'),
-
+    'migrations' => 'migrations',
     'connections' => [        
         'mysql' => [
             'driver' => 'mysql',
